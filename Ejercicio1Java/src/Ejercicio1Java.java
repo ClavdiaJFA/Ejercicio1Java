73169a145fb9ee9b688e5e5058b10366af94b712
ClavdiaJFA <cjfa08102013@gmail.com>
2021-07-16 11:32:06 -0500

    Ejercicio1 Triángulo

---
+++ Ejercicio1Java/src/Ejercicio1Java.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ejercicio1Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Introduzca su nombre:");
+		String entradaTeclado = "";
+		//Creación de un objeto Scanner
+		Scanner entradaEscaner = new Scanner (System.in);
+		//Método sobre un scanner
+		entradaTeclado = entradaEscaner.nextLine ();
+		System.out.println ("Bienvenido/a" + entradaTeclado);
+		
+	}
+
+}
